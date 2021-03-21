3bd5b68a01a316098110b622612997e762853f43
Bálint Berente <balint.berente@gmail.com>
2021-03-21 21:35:47 +0100

    Coordinates and custom area name

--- client/script.js
+++ client/script.js
@@ -1,38 +1,98 @@
+var lat, lon, area;
+var settingsOpen = false;
+
 async function getData() {
-    var result = await fetch("http://localhost:3000/data", { method: "POST", headers: {"Content-Type":"application/json"} })
+    var result = await fetch(location.href+"data", { method: "POST", headers: {"Content-Type":"application/json"}, body:JSON.stringify({"lat":lat,"lon":lon}) });
     return result.json();
 }
 
 async function createFields() {
     var content = document.getElementById("content");
     getData().then(data => {
-        if (!Array.isArray(data.departures)) return;
+        if(data === "Hiba"){
+            displayError("A szerver hibát dobott");
+            return;
+        }
+        if (!Array.isArray(data.departures)){
+            displayError("Érvénytelen adat érkezett");
+            return;
+        }
+        dismissError();
+        document.getElementById("mainTitle").innerHTML = `Közeli indulások: ${area === "" ? data.areaName : area} környéke`
         content.innerHTML = '';
-        data.departures.forEach(row => {
-            let departureTime = Math.floor((row.predicted * 1000 - Date.now()) / 60000);
-            if(departureTime < 1){
-                departureTime = "azonnal indul";
-            }else departureTime += " perc";
-            let html = `<div class="field">
-            <div class="line fieldElement">
-                <img class="lineImg" src="./svg/${row.type}.svg"></img>
-                <div class="lineNumber ${row.type.toLowerCase()}Number ${row.type==="SUBWAY"?row.line.toLowerCase()+'LineNumber':''}">
-                    <p>${row.type === "SUBWAY" ? row.line.charAt(1) : row.line}</p>
+        if(data.departures.length>0){
+            data.departures.forEach(row => {
+                let departureTime = Math.floor((row.predicted * 1000 - Date.now()) / 60000);
+                if(departureTime < 1){
+                    departureTime = "azonnal indul";
+                }else departureTime += " perc";
+                let html = `<div class="field">
+                <div class="line fieldElement">
+                    <img class="lineImg" src="./svg/${row.type}.svg"></img>
+                    <div class="lineNumber ${row.type.toLowerCase()}Number ${row.type==="SUBWAY" || row.type==="RAIL"?row.line.toLowerCase()+'LineNumber':''}">
+                        <p>${row.type === "SUBWAY" || row.type === "RAIL" ? row.line.charAt(1) : row.line}</p>
+                    </div>
                 </div>
-            </div>
-            <div class="destination fieldElement">
-                <p>${row.headsign}</p>
-            </div>
-            <div class="time fieldElement">
-                <p>${departureTime}</p>
-            </div>
-        </div>`
-        content.insertAdjacentHTML("beforeend", html);
-        });
-    });
+                <div class="destination fieldElement">
+                    <p>${row.headsign}</p>
+                </div>
+                <div class="time fieldElement">
+                    <p>${departureTime}</p>
+                </div>
+            </div>`
+            content.insertAdjacentHTML("beforeend", html);
+            });
+        }else{
+            let html = `<p>Nincs a közelben indulás</p>`;
+            content.insertAdjacentHTML("beforeend", html);
+        }
+    }).catch(err=>{
+        displayError("Megszakadt a kapcsolat a szerverrel!");
+    })
+}
+
+function displayError(error) {
+    document.getElementById("app").style.opacity="0.3;"
+    document.getElementById("errorMessage").innerHTML = error;
+    document.getElementById("errorContainer").style.display = "flex";
+}
+
+function dismissError() {
+    document.getElementById("app").style.opacity="1;"
+    document.getElementById("errorContainer").style.display = "none";
+}
+
+function saveData() {
+    lat = document.getElementById("latInput").value;
+    lon = document.getElementById("lonInput").value;
+    area = document.getElementById("areaInput").value;
+    localStorage.setItem("lat", lat.toString());
+    localStorage.setItem("lon", lon.toString());
+    localStorage.setItem("area", area);
+}
+
+function restoreData() {
+    lat = localStorage.getItem("lat") || "47.489612";
+    lon = localStorage.getItem("lon") || "19.062144";
+    area = localStorage.getItem("area") || "";
+    document.getElementById("latInput").value = lat;
+    document.getElementById("lonInput").value = lon;
+    document.getElementById("areaInput").value = area;
+}
+
+function toggleSettings() {
+    let node = document.getElementById("settingsPanelContainer");
+    if(settingsOpen){
+        settingsOpen = false;
+        node.style.display = "none";
+    }else{
+        settingsOpen = true;
+        node.style.display = "flex";
+    }
 }
 
 window.onload = function () {
+    restoreData();
     createFields();
     setInterval(createFields, 10000);
 }
--- index.js
+++ index.js
@@ -5,20 +5,25 @@ const fs = require("fs");
 const express = require("express");
 const config = require("./config.json");
 const app = express();
-const api_url = new URL("https://futar.bkk.hu/api/query/v1/ws/otp/api/where/arrivals-and-departures-for-location.json");
-api_url.searchParams.append("lon", config.lon);
-api_url.searchParams.append("lat", config.lat);
-api_url.searchParams.append("clientLon", config.lon);
-api_url.searchParams.append("clientLat", config.lat);
-api_url.searchParams.append("minutesBefore", "0");
-api_url.searchParams.append("limit", "30");
-api_url.searchParams.append("groupLimit", "2");
-api_url.searchParams.append("onlyDepartures", "true");
-api_url.searchParams.append("radius", "150");
 
-function getData() {
+
+function getUrl(lat, lon) {
+    let api_url = new URL("https://futar.bkk.hu/api/query/v1/ws/otp/api/where/arrivals-and-departures-for-location.json");
+    api_url.searchParams.append("lon", lon);
+    api_url.searchParams.append("lat", lat);
+    api_url.searchParams.append("clientLon", lon);
+    api_url.searchParams.append("clientLat", lat);
+    api_url.searchParams.append("minutesBefore", "0");
+    api_url.searchParams.append("limit", "30");
+    api_url.searchParams.append("groupLimit", "1");
+    api_url.searchParams.append("onlyDepartures", "true");
+    api_url.searchParams.append("radius", "150");
+    return api_url;
+}
+
+function getData(lat, lon) {
     return new Promise((resolve, reject) => {
-        https.get(api_url, (res) => {
+        https.get(getUrl(lat, lon), (res) => {
             var str = '';
             res.on('data', (chunk) => {
                 str += chunk;
@@ -26,10 +31,10 @@ function getData() {
             res.on('end', () => {
                 var parsed = JSON.parse(str);
                 var parsedData = parseData(parsed.data)
-                // fs.writeFileSync("./result.json",str);
-                parsedData.departures = sortData(parsedData.departures);
                 if (parsedData !== false) {
-                    resolve(parsedData);
+                    resolve(parsedData);parsedData.areaName = getAreaName(parsed);
+                    // fs.writeFileSync("./result.json",str);
+                    parsedData.departures = sortData(parsedData.departures);
                 } else reject("Hiba");
             })
             res.on('error', (err) => {
@@ -39,10 +44,10 @@ function getData() {
     })
 }
 
-function sortData(array){
+function sortData(array) {
     let len = array.length;
     for (let i = 0; i < len; i++) {
-        for (let j = 0; j < len-1; j++) {
+        for (let j = 0; j < len - 1; j++) {
             if (array[j].predicted > array[j + 1].predicted) {
                 let tmp = array[j];
                 array[j] = array[j + 1];
@@ -53,23 +58,32 @@ function sortData(array){
     return array;
 }
 
+function getAreaName(data) {
+    try {
+        let stopId = data.data.list[0].stopTimes[0].stopId;
+        return data.data.references.stops[stopId].name;
+    } catch (error) {
+        return "Ismeretlen hely"
+    }
+}
+
 function parseData(data) {
     var parsedData = {
         departures: []
     };
     try {
         data.list.forEach(element => {
-            element.stopTimes.forEach(st=>{
+            element.stopTimes.forEach(st => {
                 let record = {
                     type: data.references.routes[element.routeId].type,
                     line: data.references.routes[element.routeId].shortName,
                     headsign: st.stopHeadsign,
                     scheduled: st.departureTime,
-                    predicted: st.predictedDepartureTime || st.departureTime,
+                    predicted: st.predictedDepartureTime || st.departureTime,
                 }
                 parsedData.departures.push(record);
-        })
-            
+            })
+
         });
         return parsedData;
     } catch (error) {
@@ -78,12 +92,18 @@ function parseData(data) {
     }
 }
 
+app.use(express.json());
+
 app.post("/data", (req, res) => {
-    getData().then(result => {
-        res.send(result);
-    }).catch(err => {
-        res.send(err);
-    })
+    if (req.body.lat && req.body.lon) {
+        getData(req.body.lat, req.body.lon).then(result => {
+            res.send(result);
+        }).catch(err => {
+            res.send("Hiba");
+        })
+    } else {
+        res.send("Koordinátákat nem kapott.")
+    }
 })
 
 app.use(express.static("./client"));
