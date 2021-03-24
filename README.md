cbb2776d9aa51eaf42415fe5b2cb9d85a5ffb13f
Bálint Berente <balint.berente@gmail.com>
2021-03-24 11:42:48 +0100

    Auto-location

--- README.md
+++ README.md
@@ -14,3 +14,6 @@ BKK said that its official API will be published around Q3, 2021.
 ## Images
 **Most images found in the client/svg folder belong to BKK!**
 Some icons were found on Tabler Icons and they were free.
+## Location
+The app can determine your location, which is NOT saved on the server or in the browser. If you enable location in the app settings, you won't be able to manually edit the location or area fields. It can be turned off at the same place.
+**Safari** does not allow location over HTTP, it needs HTTPS!
--- client/script.js
+++ client/script.js
@@ -1,11 +1,38 @@
-var lat, lon, area;
+var lat, lon, area, locationEnabled;
 var settingsOpen = false;
-
-async function getData() {
-    var result = await fetch(location.href+"data", { method: "POST", headers: {"Content-Type":"application/json"}, body:JSON.stringify({"lat":lat,"lon":lon}) });
-    return result.json();
+/**
+ * Gets data from the server.
+ * @returns Data in JSON
+ */
+function getData() {
+    return new Promise((resolve, reject) => {
+        if (locationEnabled) {
+            navigator.geolocation.getCurrentPosition(async (geodata) => {
+                fetch(location.href + "data", { method: "POST", headers: { "Content-Type": "application/json" }, body: JSON.stringify({ "lat": geodata.coords.latitude.toString(), "lon": geodata.coords.longitude.toString() }) }).then(result => {
+                    resolve(result.json());
+                }).catch(err => {
+                    console.log(err);
+                    reject("Lekérdezési hiba")
+                });
+            }, () => {
+                locationEnabled = false;
+                document.getElementById("locationCheckbox").checked = locationEnabled;
+                localStorage.setItem("locationEnabled", locationEnabled);
+                reject("Helymeghatározási hiba");
+            });
+        } else {
+            fetch(location.href + "data", { method: "POST", headers: { "Content-Type": "application/json" }, body: JSON.stringify({ "lat": lat, "lon": lon }) }).then(result => {
+                resolve(result.json());
+            }).catch(err => {
+                console.log(err);
+                reject("Lekérdezési hiba");
+            });
+        }    
+    })
 }
-
+/**
+ * Main functionality. Makes a request and generates the data fields (records), and then loads it to the HTML.
+ */
 async function createFields() {
     var content = document.getElementById("content");
     getData().then(data => {
@@ -19,7 +46,7 @@ async function createFields() {
         }
         dismissError();
         document.getElementById("mainTitle").innerHTML = `${
-          area === "" ? data.areaName : area
+          area === "" || locationEnabled ? data.areaName : area
         } környéke`;
         content.innerHTML = '';
         if(data.departures.length>0){
@@ -61,40 +88,71 @@ async function createFields() {
             let html = `<p>Nincs a közelben indulás</p>`;
             content.insertAdjacentHTML("beforeend", html);
         }
-    }).catch(err=>{
-        displayError("Megszakadt a kapcsolat a szerverrel!");
+    }).catch(err => {
+        displayError(err);
     })
 }
-
+/**
+ * Displays an error message. This message can be dismissed by script or by tapping/clicking the error message.
+ * @param {string} error The error message to be displayed.
+ */
 function displayError(error) {
     document.getElementById("app").style.opacity="0.3;"
     document.getElementById("errorMessage").innerHTML = error;
     document.getElementById("errorContainer").style.display = "flex";
 }
-
+/**
+ * Hides the error message.
+ */
 function dismissError() {
     document.getElementById("app").style.opacity="1;"
     document.getElementById("errorContainer").style.display = "none";
 }
-
+/**
+ * Reads data from input fields and saves them to localStorage and global variables.
+ */
 function saveData() {
     lat = document.getElementById("latInput").value;
     lon = document.getElementById("lonInput").value;
     area = document.getElementById("areaInput").value;
+    locationEnabled = document.getElementById("locationCheckbox").checked;
     localStorage.setItem("lat", lat.toString());
     localStorage.setItem("lon", lon.toString());
     localStorage.setItem("area", area);
+    localStorage.setItem("locationEnabled", locationEnabled);
+    createFields();
 }
-
+/**
+ * Reads data from localStorage and initializes global variables.
+ */
 function restoreData() {
     lat = localStorage.getItem("lat") || "47.489612";
     lon = localStorage.getItem("lon") || "19.062144";
-    area = localStorage.getItem("area") || "";
+    area = localStorage.getItem("area") || "";
+    locationEnabled = localStorage.getItem("locationEnabled");
+    if (locationEnabled === undefined || locationEnabled === null || locationEnabled === "false") {
+        locationEnabled = false;
+    } else if(locationEnabled === "true"){
+        disableLocationInput(true);
+    }
     document.getElementById("latInput").value = lat;
     document.getElementById("lonInput").value = lon;
     document.getElementById("areaInput").value = area;
+    document.getElementById("locationCheckbox").checked = locationEnabled;
 }
-
+/**
+ * 
+ * @param {boolean} bool 
+ * Disabled or re-enables the location input fields.
+ */
+function disableLocationInput(bool) {
+    document.getElementById("latInput").disabled = bool;
+    document.getElementById("lonInput").disabled = bool;
+    document.getElementById("areaInput").disabled = bool;
+}
+/**
+ * Opens or hides settings
+ */
 function toggleSettings() {
     let node = document.getElementById("settingsPanelContainer");
     if(settingsOpen){
@@ -103,6 +161,25 @@ function toggleSettings() {
     }else{
         settingsOpen = true;
         node.style.display = "flex";
+    }
+}
+/**
+ * Is called when the user clicks the locationEnable checkbox.
+ */
+function handleLocationEnableChange() {
+    var locationCheckbox = document.getElementById("locationCheckbox");
+    var value = locationCheckbox.checked;
+    if (value) {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(console.log, console.log)
+            disableLocationInput(true);
+        } else {
+            alert("Helymeghatározás nem lehetséges")
+            locationEnabled = false;
+            locationCheckbox.checked = false;
+        }
+    } else {
+        disableLocationInput(false);
     }
 }
 
