5f376686082b36fd2db26f056e6ddd20d18c92ab
Bálint Berente <balint.berente@gmail.com>
2021-04-21 18:28:47 +0200

    Made the logo act as a refresh button

--- client/script.js
+++ client/script.js
@@ -234,11 +234,15 @@ function handleWeatherBackgroundEnable() {
     document.body.className = weatherBackgroundCheckbox.checked ? "weatherEnabled" : "";
 }
 
-window.onload = function () {
-    restoreData();
+function updateAll() {
     createFields();
     updateArea();
     fetchWeather();
+}
+
+window.onload = function () {
+    restoreData();
+    updateAll();
     setInterval(createFields, 1000 * 10);       //Fetch bkk info every 10s
     setInterval(fetchWeather, 1000 * 60 * 60);  //Fetch weather every hour
     setInterval(updateArea, 1000 * 60);  //Fetch area name every minute
